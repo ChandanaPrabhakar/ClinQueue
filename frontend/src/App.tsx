26c561665a0940cb83faf22cd1fec63926871007
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-06-18 17:13:07 +0530

    doctor login page created and integrated with backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import UserLogin from "./pages/authPages/UserLogin";
 import Home from "./pages/Home";
+import DoctorLogin from "./pages/authPages/DoctorLogin";
 const router = (
   <Routes>
     <Route path="/home" element={<Home />} />
     <Route path="/user-login" element={<UserLogin />} />
+    <Route path="/doctor-login" element={<DoctorLogin />} />
   </Routes>
 );
 function App() {
