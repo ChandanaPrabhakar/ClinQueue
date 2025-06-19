fdd2a5e634da1177d1763b4a5d5305de6ea8ff6a
ChandanaPrabhakar <154395526+ChandanaPrabhakar@users.noreply.github.com>
2025-06-20 00:43:39 +0530

    updated default landing page to home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import UserLogin from "./pages/authPages/UserLogin";
 import Home from "./pages/Home";
 import DoctorLogin from "./pages/authPages/DoctorLogin";
@@ -9,6 +9,7 @@ import MyAppointments from "./pages/userPages/MyAppointments";
 import FindMyDoctor from "./pages/userPages/FindMyDoctor";
 const router = (
   <Routes>
+    <Route path="/" element={<Navigate to="/login" replace />} />
     <Route path="/home" element={<Home />} />
     <Route path="/user-login" element={<UserLogin />} />
     <Route path="/doctor-login" element={<DoctorLogin />} />
